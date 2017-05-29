e26de4a454cebe5d844079e8d8fc303ebb916831
odinAsgardian <odinmiguel97@gmail.com>
2017-05-29 13:57:07 -0300

    atualização do banco de dados

--- banco/library.sql
+++ banco/library.sql
@@ -1,84 +1,202 @@
-/*CREATE TABLE pessoas(
-    id int(11) not null AUTO_INCREMENT primary key,
-    username varchar(200) not null,
-    password varchar(255) not null,
-    nome varchar(100) not null,
-    idade int(3)not null
-);
+-- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: lib
+-- ------------------------------------------------------
+-- Server version	5.5.54-0ubuntu0.14.04.1
 
-/*CREATE TABLE alunos(
-    matricula varchar(20) not null,
-    curso varchar(100) not null,
-    periodo varchar(20) not null,
-    email varchar(100) not null,
-    endereco varchar(100) not null,
-    contato varchar(14) not null,
-    id_pessoa int(11),
-    CONSTRAINT `id_pessoa` FOREIGN KEY (id_pessoa) REFERENCES pessoas(`id`)
-);
-CREATE TABLE funcionarios(
-    siape varchar(20) not null,
-    funcao varchar(100) not null,
-    setor varchar(100) not null,
-    email varchar(100) not null,
-    endereco varchar(100) not null,
-    contato varchar(14) not null,
-    pessoa_id int(11),
-    CONSTRAINT `pessoa_id` FOREIGN KEY (pessoa_id) REFERENCES pessoas(`id`)
-);*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE editoras(
-    id int(11) not null AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(50) not null,
-    contato varchar(14) not null,
-    endereco varchar(100) not null,
-    email varchar(100) not null
-);
+--
+-- Table structure for table `categorias`
+--
 
-CREATE TABLE categorias(
-    id int(4) not null PRIMARY KEY AUTO_INCREMENT,
-    identificador int(4) not null,
-    descricao varchar(100)
-);
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `identificador` int(4) NOT NULL,
+  `descricao` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE livros(
-    id int(11) not null AUTO_INCREMENT PRIMARY KEY,
-    titulo varchar(100) not null,
-    isbn varchar(20) not null,
-    edicao varchar(50) not null,
-    autor varchar(100) not null,
-    categoria_id int(4) not null,
-    editora_id int(11),
-    CONSTRAINT `categoria_id` FOREIGN KEY (categoria_id) REFERENCES categorias(`id`),
-    CONSTRAINT `editora_id` FOREIGN KEY (editora_id) REFERENCES editoras(`id`)
-);
+--
+-- Dumping data for table `categorias`
+--
 
-CREATE TABLE livro_fisico(
-    livros_id int(11) not null,
-    alugado boolean not null,
-    quantidade int(11) not null,
-    codigo_livro varchar(14) not null PRIMARY KEY,
-    CONSTRAINT `livros_id` FOREIGN KEY (livros_id) REFERENCES livros(`id`)
-);
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,700,'programação'),(2,124,'ética');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE users (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(150),
-    matricula VARCHAR(14),
-    email VARCHAR(30),
-    username VARCHAR(50),
-    password VARCHAR(255),
-    role VARCHAR(200),
-    created DATETIME DEFAULT NULL,
-    modified DATETIME DEFAULT NULL
+--
+-- Table structure for table `editoras`
+--
 
-);
-CREATE TABLE emprestimos(
-    id int(11) not null AUTO_INCREMENT primary key,
-    livro_id int(11) not null,
-    id_usuario int(11) not null,
-    codigo_livros int(10),
-    CONSTRAINT `livro_id` FOREIGN KEY (livro_id) REFERENCES livros(`id`),
-    CONSTRAINT `codigo_livros` FOREIGN KEY (codigo_livros) REFERENCES livro_fisico(`codigo_livro`),
-    CONSTRAINT `id_usuario` FOREIGN KEY (id_usuario) REFERENCES users(`id`)
-);
+DROP TABLE IF EXISTS `editoras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `editoras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `contato` varchar(14) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editoras`
+--
+
+LOCK TABLES `editoras` WRITE;
+/*!40000 ALTER TABLE `editoras` DISABLE KEYS */;
+INSERT INTO `editoras` VALUES (1,'cultura','0800','rua','cultura@cultura.com');
+/*!40000 ALTER TABLE `editoras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emprestimos`
+--
+
+DROP TABLE IF EXISTS `emprestimos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `emprestimos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `livro_id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `codigo_livros` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `livro_id` (`livro_id`),
+  KEY `codigo_livros` (`codigo_livros`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `livro_id` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`id`),
+  CONSTRAINT `codigo_livros` FOREIGN KEY (`codigo_livros`) REFERENCES `livro_fisico` (`codigo_livro`),
+  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emprestimos`
+--
+
+LOCK TABLES `emprestimos` WRITE;
+/*!40000 ALTER TABLE `emprestimos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `emprestimos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livro_fisico`
+--
+
+DROP TABLE IF EXISTS `livro_fisico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `livro_fisico` (
+  `livros_id` int(11) NOT NULL,
+  `alugado` tinyint(1) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `codigo_livro` int(10) NOT NULL,
+  PRIMARY KEY (`codigo_livro`),
+  KEY `livros_id` (`livros_id`),
+  CONSTRAINT `livros_id` FOREIGN KEY (`livros_id`) REFERENCES `livros` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livro_fisico`
+--
+
+LOCK TABLES `livro_fisico` WRITE;
+/*!40000 ALTER TABLE `livro_fisico` DISABLE KEYS */;
+INSERT INTO `livro_fisico` VALUES (1,0,2,0);
+/*!40000 ALTER TABLE `livro_fisico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livros`
+--
+
+DROP TABLE IF EXISTS `livros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `isbn` varchar(20) NOT NULL,
+  `edicao` varchar(50) NOT NULL,
+  `autor` varchar(100) NOT NULL,
+  `categoria_id` int(4) NOT NULL,
+  `editora_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`),
+  KEY `editora_id` (`editora_id`),
+  CONSTRAINT `categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  CONSTRAINT `editora_id` FOREIGN KEY (`editora_id`) REFERENCES `editoras` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livros`
+--
+
+LOCK TABLES `livros` WRITE;
+/*!40000 ALTER TABLE `livros` DISABLE KEYS */;
+INSERT INTO `livros` VALUES (1,'teste','teste','2','teste',2,1),(2,'programação','12345','2','teste',1,1);
+/*!40000 ALTER TABLE `livros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `matricula` varchar(14) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(200) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'obede','2345','obede@obede.com','obede','$2y$10$fN6wNMeXCjhHYhYrf2IQ.uZe43L9ZUDUwFEfBwp5jKFi0PaFXl/Se','Librarian','2017-05-25 15:57:17','2017-05-25 15:57:17'),(2,'odin123','123','odin@odin.com','odin','$2y$10$VHlogdJFynjjXJYK9QRLXOkUQPvQ1KYnZmCsCjj7Yg2QhdV2NCzVq','Librarian','2017-05-26 20:05:50','2017-05-26 20:05:50');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-29 13:54:28
--- src/Template/Users/add.ctp
+++ src/Template/Users/add.ctp
@@ -3,29 +3,41 @@
   * @var \App\View\AppView $this
   */
 ?>
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
-    <ul class="side-nav">
-        <li class="heading"><?= __('Actions') ?></li>
-        <li><?= $this->Html->link(__('List Users'), ['action' => 'index']) ?></li>
-    </ul>
-</nav>
-<div class="users form large-9 medium-8 columns content">
-    <?= $this->Form->create($user) ?>
-    <fieldset>
-        <legend><?= __('Add User') ?></legend>
-        <?php
-            echo $this->Form->control('nome');
-            echo $this->Form->control('matricula');
-            echo $this->Form->control('email');
-            echo $this->Form->control('username');
-            echo $this->Form->control('password');
-            echo $this->Form->control('role', [
-                                    'options' => ['Students' => 'Aluno',
-                                        'Employee' => 'Administrador',
-                                        'Teacher' => 'Professor',
-                                        'Librarian' => 'bibliotecária'] ]);
-        ?>
-    </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
-    <?= $this->Form->end() ?>
+<div class="container-fluid" id="actions-sidebar">
+    <div class="row content">
+        <div class="col-sm-3 sidenav">
+            <ul class="nav nav-pills nav-stacked">
+                <li class="heading"><?= __('Actions') ?></li>
+                <li><?= $this->Html->link(__('List Users'), ['action' => 'index']) ?></li>
+            </ul>
+            
+        </div>       
+    
+
+        <div class="col-sm-9">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend><?= __('Add User') ?></legend>
+                <form class="form">
+                <?php
+                    echo $this->Form->control('nome', ['class' => 'form-control', 'placeholder' => 'nome']);
+                    echo $this->Form->control('matricula', ['class' => 'form-control', 'placeholder' => 'matricula']);
+                    echo $this->Form->control('email', ['class' => 'form-control', 'placeholder' => 'email']);
+                    echo $this->Form->control('username', ['class' => 'form-control', 'placeholder' => 'usuario']);
+                    echo $this->Form->control('password', ['class' => 'form-control', 'placeholder' => 'senha']);
+                    echo "<br>";
+                    echo $this->Form->control('role', [
+                                            'options' => ['Students' => 'Aluno',
+                                                'Employee' => 'Administrador',
+                                                'Teacher' => 'Professor',
+                                                'Librarian' => 'bibliotecária'], ['class' => 'form-control']]);
+                ?>
+                    
+                </form>
+                <br>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
 </div>
